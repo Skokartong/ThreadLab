b91b5096bad78a5af093caa2a793c30c2508fedd
Skokartong <amanda.olving@gmail.com>
2023-12-17 09:43:05 +0100

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Timers;
+
+namespace ThreadLab
+{
+    public class Car
+    {
+        public string Name { get; set; }
+        public double DistanceCovered { get; set; }
+        public double Speed { get; set; }
+        public bool RaceFinished { get; set; }
+        private static readonly object lockObject = new object();
+
+        public Car(string name, double speed)
+        {
+            Name = name;
+            Speed = speed;
+            RaceFinished = false;
+        }
+
+        public void RacingCars()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            Random random = new Random();
+            System.Timers.Timer eventTimer = new System.Timers.Timer();
+
+            // Generating random events every 5 seconds
+            eventTimer.Interval = 5000;
+            eventTimer.AutoReset = true;
+            eventTimer.Elapsed += (sender, e) =>
+            {
+                int rnd = random.Next(1, 51);
+
+                if (rnd == 10)
+                {
+                    Thread.Sleep(3000);
+                    Speed -= 10;
+                    Console.WriteLine($"Oh no! {Name} car ran out of gas. Needs to refuel!");
+                    Console.WriteLine($"Speed reduced to {Speed} m/s");
+                }
+
+                else if (rnd == 20 || rnd == 50 || rnd == 49)
+                {
+                    Thread.Sleep(4000);
+                    Speed -= 8;
+                    Console.WriteLine($"Puncture for {Name}. Needs to change tires!");
+                    Console.WriteLine("Slowing down 4 seconds");
+                }
+
+                else if (rnd == 30 || rnd == 40 || rnd == 43)
+                {
+                    Console.WriteLine($"{Name} has a dirty windshield. Needs a carwash!");
+                    Console.WriteLine("Slowing down 5 seconds");
+                    Thread.Sleep(5000);
+                    Speed -= 5;
+                }
+
+                else if (rnd == 1 || rnd == 15)
+                {
+                    Speed -= 5;
+                    Console.WriteLine($"{Name} has engine trouble. Speed reduced to {Speed} m/s!");
+                }
+
+                // Check if distance is greater than or equal to 10000 after an event
+                if (DistanceCovered >= 10000)
+                {
+                    lock (lockObject)
+                    {
+                        if (!RaceFinished)
+                        {
+                            RaceFinished = true;
+                            Console.WriteLine($"Race finished! {Name} reached the goal.");
+                            eventTimer.Stop();
+                            eventTimer.Dispose();
+                        }
+                    }
+                }
+            };
+
+            eventTimer.Start();
+
+            while (true)
+            {
+                if (RaceFinished)
+                    break;
+
+                stopwatch.Start();
+                TimeSpan elapsed = stopwatch.Elapsed;
+
+                // Check if distance is greater than or equal to 10000 before updating distance
+                if (DistanceCovered >= 10000)
+                {
+                    lock (lockObject)
+                    {
+                        if (!RaceFinished)
+                        {
+                            RaceFinished = true;
+                            Console.WriteLine($"Race finished! {Name} reached the goal.");
+                            eventTimer.Stop();
+                            eventTimer.Dispose();
+                        }
+                    }
+                    break;
+                }
+
+                // Continuing to update distance
+                DistanceCovered += Speed * elapsed.TotalSeconds;
+
+                Console.WriteLine($"{Name} is racing!");
+                Console.WriteLine($"Elapsed time: {elapsed}");
+
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ThreadLab
+{
+    public class Program
+    {
+        // Program with cars (threads) competing with each other
+        public static void Main(string[] args)
+        {
+            Car car1 = new Car("Volvo", 30);
+            Car car2 = new Car("Audi", 30);
+            Car car3 = new Car("Tesla", 30);
+
+            Thread car1Thread = new Thread(car1.RacingCars);
+            Thread car2Thread = new Thread(car2.RacingCars);
+            Thread car3Thread = new Thread(car3.RacingCars);
+
+            car1Thread.Start();
+            car2Thread.Start();
+            car3Thread.Start();
+
+            bool raceFinished = false;
+
+            Console.WriteLine("Press enter to check status of cars!");
+
+            while (!raceFinished)
+            {
+                if (Console.ReadKey().Key == ConsoleKey.Enter)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"Status:");
+                    Console.WriteLine($"{car1.Name} has driven {car1.DistanceCovered} m and speed is {car1.Speed} m/s");
+                    Console.WriteLine($"{car2.Name} has driven {car2.DistanceCovered} m and speed is {car2.Speed} m/s");
+                    Console.WriteLine($"{car3.Name} has driven {car3.DistanceCovered} m and speed is {car3.Speed} m/s");
+                    Console.WriteLine();
+
+                    // Checking if any car is finished
+                    raceFinished = car1.RaceFinished || car2.RaceFinished || car3.RaceFinished;
+
+                    if (raceFinished)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Race finished!");
+
+                        if (car1.RaceFinished)
+                        {
+                            Console.WriteLine($"{car1.Name} won the race!");
+                            break;
+                        }
+
+                        else if (car2.RaceFinished)
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine($"{car2.Name} won the race!");
+                            break;
+                        }
+
+                        else
+                        {
+                            Console.WriteLine($"{car3.Name} won the race!");
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
